1f319b7d99340730bc6cc55495187634a8ea6581
Sergio Gutierrez <sergio.gutierrez@sjsu.edu>
2020-08-29 20:20:32 -0700

    First commit. Project Created. Homework1

---
+++ src/edu/sjsu/gutierrez/Homework1/FishMessage.java
@@ -0,0 +1,31 @@
+package edu.sjsu.gutierrez.Homework1;
+
+/*
+ * Sergio Gutierrez
+ * CS151
+ * 8/229/2020
+ * The program automatically prints a Fish saying "Hello World!!!"
+ */
+
+public class FishMessage
+{
+    public static void main(String[] args)
+    {
+        System.out.println("                         _/\\__                                      ");
+        System.out.println(" .                     _/     \\__                                   ");
+        System.out.println(" |\\_                __|   __ __  \\                                  ");
+        System.out.println("  \\ \\_____        ./  _ ''     \"-'\\___            _____________     ");
+        System.out.println(" __|      \\__     | .                  \\_        /             \\    ");
+        System.out.println(" \\__  ======= \\._.'                       \\\".    /               \\   ");
+        System.out.println("    \\_   ====== .        .'       )))))    @'._/                 |  ");
+        System.out.println("      |   =====  .      .        ))))))))    (_  Hello World!!!  |  ");
+        System.out.println("       '|  ====. '   ..._          ))))   _-'  \\                 |  ");
+        System.out.println("         / .===  |.--- .-:;..______.----'       \\               /   ");
+        System.out.println("       _|  _/\"\"\"\"    |    __/                    \\_____________/    ");
+        System.out.println("      /  _/         /   __/                                         ");
+        System.out.println("     |_/'           \\__/                                            ");
+
+
+    }
+
+}
